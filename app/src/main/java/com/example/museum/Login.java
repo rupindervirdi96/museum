5df84facc0c29c49ad94d27b1906e309de9ed81b
Rupinder Virdi <35664372+rockingvirdi96@users.noreply.github.com>
2020-02-27 14:19:34 -0500

    design Improved

--- app/src/main/java/com/example/museum/Login.java
+++ app/src/main/java/com/example/museum/Login.java
@@ -2,7 +2,10 @@ package com.example.museum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -11,4 +14,8 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
+
+
 }
--- app/src/main/java/com/example/museum/MainActivity.java
+++ app/src/main/java/com/example/museum/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.museum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btnLogin);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i=new Intent(this,Login.class);
+        startActivity(i);
     }
 }
